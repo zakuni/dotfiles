16da20a4076fc7b684fcc40ab0af342444a36d23
zakuni <kunio038@gmail.com>
2016-09-08 23:49:45 +0900

    change keybind of emacs

--- .emacs.d/inits/01_keybinds.el
+++ .emacs.d/inits/01_keybinds.el
@@ -6,5 +6,6 @@
 (global-set-key "\C-u" 'undo)
 
 ;; helm ショートカット
-(global-set-key (kbd "C-]") 'helm-mini) 
+(global-set-key (kbd "C-;") 'helm-mini) 
 (global-set-key (kbd "C-'") 'helm-mini)
+(global-set-key (kbd "M-x") 'helm-M-x)
