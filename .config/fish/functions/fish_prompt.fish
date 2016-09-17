d7a28b12c60f520a462a0e94eaad125274a70711
zakuni <kunio038@gmail.com>
2016-09-17 23:38:55 +0900

    configure fish by oh-my-fish

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,8 +1,1 @@
-function fish_prompt
-	test $SSH_TTY; and printf (set_color red)(whoami)(set_color white)'@'(set_color yellow)(hostname)' '
-
-    test $USER = 'root'; and echo (set_color red)"#"
-
-    # Main
-	echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
-end
+/Users/zakuni/.local/share/omf/themes/default/fish_prompt.fish
