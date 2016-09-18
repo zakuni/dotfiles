69107b12dab61d1f16ed3561f7079fd2bb9aabea
zakuni <kunio038@gmail.com>
2016-09-18 15:38:12 +0900

    configure eslint with emacs

---
+++ .emacs.d/inits/30_flycheck.el
@@ -0,0 +1,14 @@
+(global-flycheck-mode)
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(json-jsonlist)))
