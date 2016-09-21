b51337e58b42468d5a2c12fb757f785993d76439
zakuni <kunio038@gmail.com>
2016-09-21 18:50:08 +0900

    add automatically added config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,12 @@
 (custom-set-variables
- '(split-width-threshold 100)
-)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3b0a350918ee819dca209cec62d867678d7dac74f6195f5e3799aa206358a983" default)))
+ '(split-width-threshold 100))
 
 (add-to-list 'load-path "~/.emacs.d/plugins/")
 (add-to-list 'load-path "~/.emacs.d/themes/")
@@ -29,3 +35,9 @@
 (setq ns-alternate-modifier (quote super))
 
 )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((((class color) (background dark)) (:background "#775511")) (((class color) (background light)) (:background "555555")))))
