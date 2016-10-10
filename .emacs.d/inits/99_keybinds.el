3bc8a133feb1adadd855d41b1f08eefac1f306f0
zakuni <kunio038@gmail.com>
2016-10-10 17:34:36 +0900

    use ido(and smex)

--- .emacs.d/inits/99_keybinds.el
+++ .emacs.d/inits/99_keybinds.el
@@ -8,6 +8,8 @@
 ; undoショートカットキー
 (global-set-key "\C-u" 'undo)
 
+(global-set-key (kbd "M-x") 'smex)
+
 ;(setq indent-line-function 'indent-relative-maybe)
 (global-set-key "\C-m" 'newline-and-indent); Returnキーで改行＋オートインデント
 ;(global-set-key "\C-m" 'indent-new-comment-line); Returnキーで改行＋オートインデント＋コメント行
