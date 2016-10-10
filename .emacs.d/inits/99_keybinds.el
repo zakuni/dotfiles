5e61dbde470dd6939c59a25024d97fb8c678532c
zakuni <kunio038@gmail.com>
2016-10-10 17:00:26 +0900

    stop using helm

--- .emacs.d/inits/99_keybinds.el
+++ .emacs.d/inits/99_keybinds.el
@@ -8,13 +8,6 @@
 ; undoショートカットキー
 (global-set-key "\C-u" 'undo)
 
-;; helm ショートカット
-(global-set-key (kbd "C-]") 'helm-mini)
-(global-set-key (kbd "C-'") 'helm-mini)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-x C-d") 'helm-browse-project)
-
 ;(setq indent-line-function 'indent-relative-maybe)
 (global-set-key "\C-m" 'newline-and-indent); Returnキーで改行＋オートインデント
 ;(global-set-key "\C-m" 'indent-new-comment-line); Returnキーで改行＋オートインデント＋コメント行
