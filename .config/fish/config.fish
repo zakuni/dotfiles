3b930c9de51d4b2ec37e86452099b4f7a2625971
zakuni <kunio038@gmail.com>
2017-07-23 22:49:01 +0900

    update config.fish
    use omf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,6 @@
-if status --is-interactive
-    if test -z (echo $TMUX)
-        if not test (tmux attach)
-            tmux new-session
-        end
-    end
-end
-
 alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
 
 set -x LANG ja_JP.UTF-8
-
-# Heroku Toolbelt
-set -x PATH /usr/local/heroku/bin $PATH
 
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
