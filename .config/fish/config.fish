c54e2fdc654558a16bbb9d4d6e399026532216f7
zakuni <kunio038@gmail.com>
2019-08-14 18:06:56 +0900

    upgrade fish config
    use init.fish rather than config.fish

--- .config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
-
-set -x LANG ja_JP.UTF-8
-
-set -x GOPATH $HOME/go
-set -x PATH $PATH $GOPATH/bin
--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -10,3 +10,11 @@ end
 
 # https://github.com/oh-my-fish/theme-default/issues/8#issuecomment-287571670
 set fish_pager_color_progress cyan
+
+alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
+
+set -x LANG ja_JP.UTF-8
+
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+set -g fish_user_paths "/usr/local/opt/avr-gcc@7/bin" $fish_user_paths
