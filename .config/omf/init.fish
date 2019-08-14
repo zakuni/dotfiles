e0b9756f38af93321be7ed79aaf6f43df31760e3
zakuni <kunio038@gmail.com>
2019-08-14 20:49:31 +0900

    Update init.fish
    - don't open tmux on  fish start
    - add tmux shell integration

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,13 +1,5 @@
 set -xg PATH ~/anaconda/bin $PATH
 
-if status --is-interactive
-    if test \( -z (echo $TMUX) \) -a \( -z (echo $VSCODE_PID) \)
-        if not test (tmux attach)
-            tmux new-session
-        end
-    end
-end
-
 # https://github.com/oh-my-fish/theme-default/issues/8#issuecomment-287571670
 set fish_pager_color_progress cyan
 
@@ -18,3 +10,4 @@ set -x LANG ja_JP.UTF-8
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
 set -g fish_user_paths "/usr/local/opt/avr-gcc@7/bin" $fish_user_paths
+source ~/.iterm2_shell_integration.(basename $SHELL)
